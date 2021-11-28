1b5d2eef81775c70736b148ce31a2c235475af99
Tamanna garg <tgarg661@gmail.com>
2021-11-28 12:06:17 +0530

    done changes

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
     // Write your code here
-}
+    const hash={}
+    index=0
+    while(index<arr.length){
+        hash[arr[index]]=arr[index+1]
+        index+=2
 
+    }
+    return hash
+    
+}
 module.exports = CreateObject;
